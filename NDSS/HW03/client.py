83afca25df907b7f3af307fced633ade83c0ea88
akash896 <akashmondal896@gmail.com>
2021-12-12 12:18:28 +0530

    Dual MAC clculation and single time message transfer done

--- NDSS/HW03/client.py
+++ NDSS/HW03/client.py
@@ -15,23 +15,39 @@ def generate_asymmetric_keys():
     client_Secret_Key = random.randrange(1, curve.n)
     client_Public_Key = scalar_mult(client_Secret_Key, curve.g)
 
+
+
 def check_server_message(s, server_shared_secret):
     s_to_c_message = s.recv(1024).decode()
     hash = s_to_c_message[-64:]
     message = s_to_c_message[0:len(s_to_c_message)-64]
-    calculated_h = hmac.new(str(server_shared_secret[0]).encode(), message.encode(), hashlib.sha256)
-    calculated_hash = calculated_h.hexdigest()
-    if calculated_hash == hash:
-        print("S to C message SUCCESSFULLY transferred ####################################")
+    calculated_h1 = hmac.new(str(server_shared_secret[0]).encode(), message.encode(), hashlib.sha256)
+    calculated_hash1 = calculated_h1.hexdigest()
+    appended_message_hash1 = message + calculated_hash1
+    print("\n Message part S=>C = \n", message, "\n MAC1 \n", calculated_hash1)
+    calculated_h2 = hmac.new(str(server_shared_secret[1]).encode(), appended_message_hash1.encode(), hashlib.sha256)
+    calculated_hash2 = calculated_h2.hexdigest()
+    if calculated_hash2 == hash:
+        print("\n message S=>C = \n", message, "\n calculated_MAC2 = \n", calculated_hash2)
+        print("\nS to C message SUCCESSFULLY transferred ####################################\n")
     else:
+        print("\n message S=>C = \n", message, "\n calculated_MAC2 = \n", calculated_hash2)
         print("server to client MESSAGE is NOT CORRECT")
+
+
 
 def send_message_to_server(s, server_shared_secret):
     c_to_s_message = input("Enter essage for Server")
-    h = hmac.new(str(server_shared_secret[0]).encode(), c_to_s_message.encode(), hashlib.sha256)
-    hash = str(h.hexdigest())
-    message_plus_hash = c_to_s_message + hash
-    s.send(message_plus_hash.encode())
+    h1 = hmac.new(str(server_shared_secret[0]).encode(), c_to_s_message.encode(), hashlib.sha256)
+    hash1 = str(h1.hexdigest())
+    message_plus_hash1 = c_to_s_message + hash1
+    print("\n C=> S message = \n", c_to_s_message, "\n MAC1 = \n ", hash1)
+    h2 = hmac.new(str(server_shared_secret[1]).encode(), message_plus_hash1.encode(), hashlib.sha256)
+    hash2 = str(h2.hexdigest())
+    message_plus_hash2 = c_to_s_message + hash2
+    print("\n C=> S message + MAC1 = \n", message_plus_hash1, "\n MAC2 = \n ", hash2)
+    print("\n Final message sent from C=> S = \n", message_plus_hash2)
+    s.send(message_plus_hash2.encode())
 
 
 
--- NDSS/HW03/server.py
+++ NDSS/HW03/server.py
@@ -16,24 +16,41 @@ def generate_asymmetric_keys():
     server_Public_Key = scalar_mult(server_Secret_Key, curve.g)
 
 def send_message_to_client(c, client_shared_secret):
-   s_to_c_message = input("Enter essage for Client")
+   s_to_c_message = input("Enter message S=> C :  \n")
    h = hmac.new(str(client_shared_secret[0]).encode(), s_to_c_message.encode(), hashlib.sha256)
-   hash = str(h.hexdigest())
-   print("message = ", s_to_c_message, " \n hash = ", hash)
-   message_hash = s_to_c_message + hash
-   c.send(message_hash.encode())
+   hash1 = str(h.hexdigest())
+   print("S->C message = ", s_to_c_message, " \n MAC1 = ", hash1)
+   message_hash1 = s_to_c_message + hash1 # message + MAC(message, key[0])
+   print("Message + MAC1 = \n", message_hash1)
+
+   #calculate MAC using 2nd shared secret
+   h = hmac.new(str(client_shared_secret[1]).encode(), message_hash1.encode(), hashlib.sha256)
+   hash2 = str(h.hexdigest())
+   print("S->C message + MAC1 = ", message_hash1, " \n MAC2 = ", hash2)
+   message_hash2 = s_to_c_message + hash2  # message + MAC(message + MAC(message, key[0]), key[1])
+   print("Final Message transferred from S => C = \n", message_hash2)
+   c.send(message_hash2.encode()) #sending message appended to dual MAC
 
 def check_server_message(c, client_shared_secret):
    c_to_s_message = c.recv(1024).decode()
+   print("\n Message received from CLIENT = ", c_to_s_message)
    hash = c_to_s_message[-64:]
    message = c_to_s_message[0:len(c_to_s_message) - 64]
-   calculated_h = hmac.new(str(client_shared_secret[0]).encode(), message.encode(), hashlib.sha256)
-   calculated_hash = calculated_h.hexdigest()
-   if calculated_hash == hash:
-      print("message C->S = ", message, "\n hash = ", hash)
-      print("C to S message SUCCESSFULLY transferred ####################################")
+   calculated_h1 = hmac.new(str(client_shared_secret[0]).encode(), message.encode(), hashlib.sha256)
+   calculated_hash1 = calculated_h1.hexdigest()
+   print("\n message part C->S = \n", message, "\n MAC1 = \n", calculated_hash1)
+   message_plus_hash1 = message + calculated_hash1
+
+   calculated_h2 = hmac.new(str(client_shared_secret[1]).encode(), message_plus_hash1.encode(), hashlib.sha256)
+   calculated_hash2 = calculated_h2.hexdigest()
+   print("message C->S + MAC1 = \n", message_plus_hash1, "\n MAC2 = \n", calculated_hash2)
+
+   if calculated_hash2 == hash:
+      print("\n message C->S = \n", message, "\n final MAC2 = \n", calculated_hash2)
+      print("#################   C => S message SUCCESSFULLY transferred   ####################################")
    else:
-      print("Client to server MESSAGE is NOT CORRECT")
+      print("\n message C->S = \n", message, "\n final MAC2 = \n", calculated_hash2)
+      print("############   Client to server MESSAGE is NOT CORRECT   ##############################")
 
 
 def start_server():
